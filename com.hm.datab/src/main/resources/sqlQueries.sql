6513e79a9e4929c57d3349af6e0c7821bef55335
aparna <aparna@PDT-003>
2017-05-27 12:58:09 +0530

    sql

--- com.hm.datab/src/main/resources/sqlQueries.sql
+++ com.hm.datab/src/main/resources/sqlQueries.sql
@@ -9,3 +9,16 @@ CREATE TABLE `hotelmanagement`.`nurse` (
   `addeddate` DATE NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`nurseid`));
+  
+  
+  #pharmacist table,Created By:Aparna,Date:26-05-2017.
+CREATE TABLE `hotelmanagement`.`pharmacist` (
+  `pharmacistid` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NOT NULL,
+  `image` BLOB NOT NULL,
+  `addeddate` DATE NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`pharmacistid`));
